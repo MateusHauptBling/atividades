ff59f136d29ca38055990225557552ef9664c9e2
MateusHauptBling <mateus.haupt@bling.com.br>
2022-06-08 15:15:03 -0300

    finalizado, adicionado id position e alterado as funções changePosition e changeObject

--- Minigame/minigame.js
+++ Minigame/minigame.js
@@ -1,76 +1,72 @@
 $(function () {
 
-	function changeObject(classe) {
-		$('div').removeClass(function (index, className) {
-			return (className.match(/circle|cirquare/g) || []).join('');
-		}).addClass(classe);
+	function changePosition(position) {
+		$('#position').removeClass('botLeft botMid botRight midLeft midMid midRight topLeft topMid topRight').addClass(position);
 	}
-	function changePosition(classe) {
-		$('div').removeClass(function (index, className) {
-			return (className.match(/(^|\s)(bot|mid|top)\S+/g) || []).join('');
-		}).addClass(classe);
+	function changeObject(type) {
+		$('#position').removeClass('circle cirquare').addClass(type);
 	}
 
 
-	$(document).keypress(function (event) {
-		event.preventDefault();
+$(document).keypress(function (event) {
+	event.preventDefault();
 
-		switch (event.keyCode) {
-			case 49:
-				changePosition('botLeft');
-				changeObject('');
-				break;
+	switch (event.keyCode) {
+		case 49:
+			changePosition('botLeft');
+			changeObject('');
+			break;
 
-			case 50:
-				changePosition('botMid');
-				changeObject('circle');
-				break;
+		case 50:
+			changePosition('botMid');
+			changeObject('circle');
+			break;
 
-			case 51:
-				changePosition('botRight');
-				changeObject('cirquare');
-				break;
+		case 51:
+			changePosition('botRight');
+			changeObject('cirquare');
+			break;
 
-			case 52:
-				changePosition('midLeft');
-				changeObject('cirquare');
-				break;
+		case 52:
+			changePosition('midLeft');
+			changeObject('cirquare');
+			break;
 
-			case 53:
-				changePosition('midMid');
-				changeObject('');
-				break;
+		case 53:
+			changePosition('midMid');
+			changeObject('');
+			break;
 
-			case 54:
-				changePosition('midRight');
-				changeObject('circle')
-				break;
+		case 54:
+			changePosition('midRight');
+			changeObject('circle')
+			break;
 
-			case 55:
-				changePosition('topLeft');
-				changeObject('circle');
-				break;
+		case 55:
+			changePosition('topLeft');
+			changeObject('circle');
+			break;
 
-			case 56:
-				changePosition('topMid');
-				changeObject('cirquare');
-				break;
+		case 56:
+			changePosition('topMid');
+			changeObject('cirquare');
+			break;
 
-			case 57:
-				changePosition('topRight');
-				changeObject('');
-				break;
+		case 57:
+			changePosition('topRight');
+			changeObject('');
+			break;
 
-			case 42:
-				$('div').removeClass('giroEsq').addClass('giroDir');
-				break;
-			case 47:
-				$('div').removeClass('giroDir').addClass('giroEsq');
-				break;
-			case 46:
-				$('div').removeClass('giroDir giroEsq');
-				break;
-		}
+		case 42:
+			$('div').removeClass('giroEsq').addClass('giroDir');
+			break;
+		case 47:
+			$('div').removeClass('giroDir').addClass('giroEsq');
+			break;
+		case 46:
+			$('div').removeClass('giroDir giroEsq');
+			break;
+	}
 
-	});
+});
 });
